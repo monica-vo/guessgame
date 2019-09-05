a359b2f613861e738f256cdce45bad965b49ddf0
monicavo7 <53335579+monicavo7@users.noreply.github.com>
2019-09-05 17:09:00 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+  <div className="App">
+      <h1>Game using React</h1>
+    <div class="imgbox img">
+      <div class="container">
+      <div class="box box1">Box 1</div>
+      <div class="box">Box 2</div>
+      <div class="box">Box 3</div>
+      <div class="box">Box 4</div>
+      <div class="box">Box 5</div>
+      <div class="box">Box 6</div>
+      <div class="box">Box 7</div>
+      <div class="box">Box 8</div>
+      <div class="box">Box 9</div>
+      </div>
+    </div>
+  </div>
+    
+  );
+}
+
+export default App;
